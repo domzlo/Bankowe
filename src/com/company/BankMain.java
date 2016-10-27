e4d18108696eec04cef2d4418704a7482d5c7804
Domi <zlotowska.dominika@gmail.com>
2016-10-27 11:22:56 +0200

    Bankowe_2

--- src/com/company/BankMain.java
+++ src/com/company/BankMain.java
@@ -8,6 +8,7 @@ public class BankMain {
     public static void main(String[] args) {
         Konto kontoAliny = new Konto(new BigInteger("11112222333344445555666677"), new BigDecimal("0"), "PLN", new Osoba("Alina", "Nowak"));
         Konto kontoKarola = new Konto("11112222333344445555666677", new BigDecimal("100"), "PLN", new Osoba("Karol", "Nowak"));
+
         kontoAliny.wpłać(new BigDecimal("1000.02"));
         kontoKarola.wpłać(new BigDecimal("5000000.02"));
 
