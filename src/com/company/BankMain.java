6397e9f40cdf75480daaf5848ea124bdc0b3394f
Domi <zlotowska.dominika@gmail.com>
2016-10-27 11:06:04 +0200

    Bankowe_2

--- src/com/company/BankMain.java
+++ src/com/company/BankMain.java
@@ -6,11 +6,12 @@ import java.math.BigInteger;
 public class BankMain {
 
     public static void main(String[] args) {
-        Konto konto = new Konto();
-        konto.numerKonta = new BigInteger("11112222333344445555666677");
+        Konto kontoAliny = new Konto(new BigInteger("11112222333344445555666677"), new BigDecimal("0"), "PLN", new Osoba("Alina", "Nowak"));
+        Konto kontoKarola = new Konto("11112222333344445555666677", new BigDecimal("100"), "PLN", new Osoba("Karol", "Nowak"));
+        kontoAliny.wpłać(new BigDecimal("1000.02"));
+        kontoKarola.wpłać(new BigDecimal("5000000.02"));
 
-        konto.wpłać(new BigDecimal("1000.02"));
-
-        System.out.println(konto.stanKonta);
+        System.out.println(kontoAliny.stanKonta);
+        System.out.println(kontoKarola.stanKonta);
     }
 }
--- src/com/company/Konto.java
+++ src/com/company/Konto.java
@@ -11,6 +11,23 @@ public class Konto {
     String waluta;
     Osoba właściciel;
 
+    public Konto(String numerKonta, BigDecimal stanKonta, String waluta, Osoba właściciel) {
+        this.numerKonta = new BigInteger(numerKonta);
+        this.stanKonta = stanKonta;
+        this.waluta = waluta;
+        this.właściciel = właściciel;
+    }
+
+    public Konto(BigInteger numerKonta, BigDecimal stanKonta, String waluta, Osoba właściciel) {
+        this.numerKonta = numerKonta;
+        this.stanKonta = stanKonta;
+        this.waluta = waluta;
+        this.właściciel = właściciel;
+    }
+//    w ten sposób konstruktor wymaga podania new BigIntegera a w wersji wyżej wymaga Stringa. W zakładce
+//    BankMain wykorzystuje oba konstruktory i działa
+
+
     public void wpłać(BigDecimal kwota) {
         stanKonta = stanKonta.add(kwota);
 //        ta meoda xxx.add odnosi się do BigDecimala - na int moglibyśmy po prosty dodać znakiem +
